79194bf7440af678d5c595aa1b397fd5127e54e9
ghamidibrahim <mihidighamidibrahim@gmail.com>
2026-04-12 14:37:57 +0100

    fix supabase v2

--- db/supabase.js
+++ db/supabase.js
@@ -1,8 +1,11 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseKey = process.env.SUPABASE_KEY || '';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+  {
+    auth: { persistSession: false }
+  }
+);
 
 module.exports = supabase;
